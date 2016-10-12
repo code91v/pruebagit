ff99adea61e10261c0682b296e32d0dc9d5ad1ed
jose <jose.florez@gmail.com>
2016-10-12 17:50:34 -0430

    add auth action

--- config/mail.php
+++ config/mail.php
@@ -56,8 +56,8 @@ return [
     */
 
     'from' => [
-        'address' => 'hello@example.com',
-        'name' => 'Example',
+        'address' => 'dev@laradev.esy.es',
+        'name' => 'dev91',
     ],
 
     /*
